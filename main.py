c8f51524ef1e96bd8dd404b8d35a3d94f002f026
Alex <sheppenguin@gmail.com>
2023-09-07 20:37:54 -0400

    wasd

--- main.py
+++ main.py
@@ -10,11 +10,42 @@ from pygame import mixer
 #Functions for the Music
 
 def chooseFile():
-    print("choose a file")
-    chosenFile = askopenfilename(filetypes=[("All Files"),"*."])
+    #print("choose a file")
+    chosenFile = askopenfilename(filetypes=[("All Files","*")])
+    print(chosenFile)
+    #Destroys the window, will be used for 
+    root.destroy()
 
 
-#GUI!
+    #For determining filetypes!
+    if chosenFile.endswith('.wav'):
+        print('mp3')
+        audioFile()
+    elif chosenFile.endswith('mp4'):
+        print('mp4')
+        videoFile()
+    else:
+        print('invalid file type')
+        
+
+def audioFile():
+    print('hi')
+    audio = Tk()
+    audio.title("Shep's Audio Player")
+    audio.geometry("1280x720")
+    audio.configure(background="#FFFFFF")
+    audio.mainloop()
+
+def videoFile():
+    video = Tk()
+    video.title("Shep's Video Player")
+    video.geometry("1280x720")
+    video.configure(background="#FFFFFF")
+    video.mainloop()
+
+
+
+#GUI FOR CHOOSE A FILE WINDOW!
 
 root = Tk()
 root.title("Shep's Tech Flex!!")
