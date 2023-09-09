eb0d59ffc62fad23ceba9f3d6b2558e292a49085
Alex <sheppenguin@gmail.com>
2023-09-09 00:16:46 -0400

    video plays!

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter as Tk
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from tkinter import messagebox
+from tkVideoPlayer import TkinterVideo
 import pygame as pygame
 from pygame import mixer
 import os
@@ -16,44 +17,60 @@ def chooseFile():
     chosenFile = askopenfilename(filetypes=[("All Files","*")])
     #print(chosenFile)
     #Destroys the window, will be used for 
-   
-
 
     #For determining filetypes!
     if chosenFile.endswith('.wav'):
         mixer.init()
         root.destroy()
-        #print('mp3')
+        #print('mp3') <-- Debug line
         audioFile(chosenFile)
     elif chosenFile.endswith('mp4'):
         root.destroy()
-        #print('mp4')
+        #print('mp4') <-- Debug line
         videoFile(chosenFile)
     else:
-        #print('invalid file type')
+        #print('invalid file type') <-- Debug line
         messagebox.showerror("Error", "Invalid file type. Please choose a \".wav\" or \".mp4\" file")
 
 
 def audioFile(chosenFile):
-    print('hi')
+    #print('hi') <-- Debug line.
+
+    #Creates window for the audio file.
     audio = Tk()
     audio.title("Shep's Audio Player")
     audio.geometry("1280x720")
     audio.configure(background="#FFFFFF")
     
 
-    print(chosenFile)
+    #print(chosenFile) <-- debug line.
 
+    #Plays the MP3 file.
     sound = mixer.Sound(chosenFile)
     sound.play()
     
+    #Opens the GUI.
     audio.mainloop()
 
 def videoFile(chosenFile):
+
+    #print('hiii) <-- Debug line.
+
+    #Video Gui!
     video = Tk()
     video.title("Shep's Video Player")
-    video.geometry("1280x720")
+    video.geometry("1920x1080")
     video.configure(background="#FFFFFF")
+
+    #Plays the MP4
+    videoplayer = TkinterVideo(master=video, scaled=True)
+    videoplayer.load(chosenFile)
+    videoplayer.pack(expand=True, fill="both")
+
+    videoplayer.play()
+
+
+    #Opens the GUI.
     video.mainloop()
 
 
@@ -70,5 +87,5 @@ root.resizable(False, False)
 fileButton = Button(root, height = 100, width=100, bg="#BC13FE", text="Choose a File", command = chooseFile)
 fileButton.pack()
 
-#runs tkinter code
+#Opens the "Choose File" Window.
 root.mainloop()
